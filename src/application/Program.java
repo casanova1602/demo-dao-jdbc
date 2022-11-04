1c114f44946b7aada40e731936684816d4f39917
Andre Casanova <casanova1602.ar@gmail.com>
2022-11-04 00:52:56 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,9 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +18,8 @@ public class Program {
 		
 		Seller obj2 = new Seller(21, "Bob", "bob@gmail.com", new Date(),
 				3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(obj2);
 	}
