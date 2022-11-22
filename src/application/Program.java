6513d42b791bca212ddd029f9b3bc5dc7f60fb01
Andre Casanova <casanova1602.ar@gmail.com>
2022-11-21 23:05:32 -0300

    Delete Implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,6 +14,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("===== TEST 1: seller findById()============");
 		Seller seller = sellerDao.findById(3);
@@ -32,6 +34,20 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new ID = " + newSeller.getId());
+		
+		System.out.println("\n ===== TEST 5: seller update()==============::|");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
+		
+		System.out.println("\n ===== TEST 5: seller update()==============::|");
+		System.out.println("Digite um id: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 
 }
